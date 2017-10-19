c225bec82bc8ec28df72e320f99b243078179b22
vdt-mik <vdt-mik@alterpost.org>
2017-10-19 15:24:48 +0300

    bug fix

--- aws/asg.sh
+++ aws/asg.sh
@@ -87,7 +87,7 @@ echo "ASG up!"
 echo "Create new instances"
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name `get_pr "ASG_NAME"` \
 --termination-policies "OldestInstance" --max-size ${NEW_SIZE} --desired-capacity ${NEW_SIZE} 
-sleep 60
+sleep 30
 echo "Kick old instances"
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name `get_pr "ASG_NAME"` --max-size `get_pr "ASG_MAX_SIZE"` --desired-capacity `get_pr "ASG_MAX_SIZE"`
 else
