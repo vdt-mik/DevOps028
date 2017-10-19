eba96afe18f955bb8148e113d30954f01f3fb44b
vdt-mik <vdt-mik@alterpost.org>
2017-10-19 14:41:26 +0300

    bug fix

--- aws/rds.sh
+++ aws/rds.sh
@@ -18,7 +18,7 @@
 function get_pr {
     aws ssm get-parameters --names $1 --with-decryption --output text | awk '{print $4}'
 }
-desc_rds
+
 if [[ "`aws rds describe-db-instances --db-instance-identifier get_pr "DB_INST_NAME" 2>/dev/null | wc -l`" != "0" ]]
 then
 echo "RDS up!"
@@ -33,14 +33,14 @@ aws rds create-db-instance --db-instance-identifier `get_pr "DB_INST_NAME"` --al
 TARGET_STATUS=available
 STATUS=unknown
 while [[ "$STATUS" != "$TARGET_STATUS" ]]; do
-        STATUS=`aws rds describe-db-instances --db-instance-identifier `get_pr "DB_INST_NAME"` | grep DBInstanceStatus | awk '{print$2}' | cut -d'"' -f2`
+        STATUS=`aws rds describe-db-instances --db-instance-identifier get_pr "DB_INST_NAME" | grep DBInstanceStatus | awk '{print$2}' | cut -d'"' -f2`
         echo "Database $INSTANCE : $STATUS ... "
         sleep 15
 done
 #======================================
 # Set DB variables
 #
-EXISTING_DB_INSTANCE_INFO=aws rds describe-db-instances --db-instance-identifier `get_pr "DB_INST_NAME"` \
+EXISTING_DB_INSTANCE_INFO=aws rds describe-db-instances --db-instance-identifier get_pr "DB_INST_NAME" \
 --query 'DBInstances[*].[DBInstanceIdentifier,Endpoint.Address,Endpoint.Port]' --output text
 aws ssm put-parameter --name "DB_HOST" --type "String" --value "$(echo ${EXISTING_DB_INSTANCE_INFO} | awk '{print $2}')" --overwrite
 aws ssm put-parameter --name "DB_PORT" --type "String" --value "$(echo ${EXISTING_DB_INSTANCE_INFO} | awk '{print $3}')" --overwrite
