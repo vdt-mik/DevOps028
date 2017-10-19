f8fa53fbda31ea379394b30d28f0b93d251eae6a
vdt-mik <vdt-mik@alterpost.org>
2017-10-19 13:49:23 +0300

    bug fix

--- aws/asg.sh
+++ aws/asg.sh
@@ -2,64 +2,82 @@
 #======================================
 # Delete ASG
 #
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $(aws ssm get-parameters --names ASG_NAME --with-decryption --output text | awk '{print $4}') --force-delete 2>/dev/null
+#aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $(aws ssm get-parameters --names ASG_NAME --with-decryption --output text | awk '{print $4}') --force-delete 2>/dev/null
 #======================================
 # Wait delete ASG
 #
-count=1
-while [[ "$count" != "3" ]]; do
-        count=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $(aws ssm get-parameters --names ASG_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`
-        echo "ASG : deleting ... "
-        sleep 5
-done
-echo "ASG deleted ===============================>"
+#count=1
+#while [[ "$count" != "3" ]]; do
+#        count=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $(aws ssm get-parameters --names ASG_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`
+#        echo "ASG : deleting ... "
+#        sleep 5
+#done
+#echo "ASG deleted ===============================>"
 #======================================
 # Delete LC
 #
-aws autoscaling delete-launch-configuration --launch-configuration-name $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null
+#aws autoscaling delete-launch-configuration --launch-configuration-name $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null
 #======================================
 # Wait delete LC
 #
-count=1
-while [[ "$count" != "3" ]]; do
-        count=`aws autoscaling describe-launch-configurations --launch-configuration-names $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`
-        echo "LC : deleting ... "
-        sleep 5
-done
-echo "LC deleted ===============================>"
+#count=1
+#while [[ "$count" != "3" ]]; do
+#        count=`aws autoscaling describe-launch-configurations --launch-configuration-names $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`
+#        echo "LC : deleting ... "
+#        sleep 5
+#done
+#echo "LC deleted ===============================>"
+function get_pr {
+    aws ssm get-parameters --names $1 --with-decryption --output text | awk '{print $4}'
+}
 #======================================
 # Create LC
 # 
-aws autoscaling create-launch-configuration --launch-configuration-name $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') \
---key-name ec2-key --image-id ami-c7ee5ca8 --security-groups samsara-sg --instance-type t2.micro --user-data file://aws/user-data.sh --instance-monitoring Enabled=true --iam-instance-profile EC2
-sleep 5
+aws autoscaling create-launch-configuration --launch-configuration-name `get_pr "LC_NAME"` --key-name ec2-key --image-id ami-c7ee5ca8 \
+--security-groups samsara-sg --instance-type t2.micro --user-data file://aws/user-data.sh --instance-monitoring Enabled=true --iam-instance-profile EC2
 echo "LC created ===============================>"
 #======================================
 # Delete LB
 #
-aws elb delete-load-balancer --load-balancer-name $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null
+#aws elb delete-load-balancer --load-balancer-name $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null
 #======================================
 # Wait delete LB
 #
-count=1
-while [[ "$count" != "0" ]]; do
-        count=`aws elb describe-load-balancers --load-balancer-names $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`
-        echo "LC : deleting ... "
-        sleep 5
-done
-echo "LB deleted ===============================>"
+#count=1
+#while [[ "$count" != "0" ]]; do
+#        count=`aws elb describe-load-balancers --load-balancer-names $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`
+#        echo "LC : deleting ... "
+#        sleep 5
+#done
+#echo "LB deleted ===============================>"
 #======================================
 # Create LB
 #
-aws elb create-load-balancer --load-balancer-name $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') \
+aws elb create-load-balancer --load-balancer-name `get_pr "LB_NAME"` \
 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=9000" --subnets subnet-13828169 --security-groups sg-835e8ee9
-aws elb configure-health-check --load-balancer-name $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') \
+aws elb configure-health-check --load-balancer-name `get_pr "LB_NAME"` \
 --health-check Target=HTTP:9000/login,Interval=5,UnhealthyThreshold=5,HealthyThreshold=2,Timeout=2
 echo "LB created ===============================>"
 #======================================
 # Create ASG
 #
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name $(aws ssm get-parameters --names ASG_NAME --with-decryption --output text | awk '{print $4}') \
---launch-configuration-name $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') --min-size 1 --max-size 3 --desired-capacity 1 --tags \
---load-balancer-names $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') --health-check-type ELB --health-check-grace-period 300 --availability-zones eu-central-1b
+#aws autoscaling create-auto-scaling-group --auto-scaling-group-name $(aws ssm get-parameters --names ASG_NAME --with-decryption --output text | awk '{print $4}') \
+#--launch-configuration-name $(aws ssm get-parameters --names LC_NAME --with-decryption --output text | awk '{print $4}') --min-size 1 --max-size 3 --desired-capacity 1 --tags \
+#--load-balancer-names $(aws ssm get-parameters --names LB_NAME --with-decryption --output text | awk '{print $4}') --health-check-type ELB --health-check-grace-period 300 --availability-zones eu-central-1b
+#echo "ASG created ===============================>"
+if [[ "aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names `get_pr "ASG_NAME"` 2>/dev/null | wc -l" != "3" ]]
+then
+NEW_SIZE=`get_pr "ASG_MAX_SIZE"`*2
+echo "$NEW_SIZE"
+echo "ASG up!"
+echo "Create new instanse"
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name `get_pr "ASG_NAME"` \
+--termination-policies "OldestInstance" --max-size ${NEW_SIZE} --desired-capacity ${NEW_SIZE} 
+else
+echo "ASG down!!!"
+echo "Starting create"
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name `get_pr "ASG_NAME"` \
+--launch-configuration-name `get_pr "LC_NAME"` --min-size 1 --max-size `get_pr "ASG_MAX_SIZE"` --desired-capacity 1 \
+--load-balancer-names `get_pr "LB_NAME"` --health-check-type ELB --health-check-grace-period 300 --availability-zones eu-central-1b
 echo "ASG created ===============================>"
+fi
--- aws/rds.sh
+++ aws/rds.sh
@@ -15,31 +15,32 @@
 #======================================
 # Create RDS instance
 #
-if [[ "`aws rds describe-db-instances --db-instance-identifier $(aws ssm get-parameters --names DB_INST_NAME --with-decryption --output text | awk '{print $4}') 2>/dev/null | wc -l`" != "0" ]]
+function get_pr {
+    aws ssm get-parameters --names $1 --with-decryption --output text | awk '{print $4}'
+}
+if [[ "`aws rds describe-db-instances --db-instance-identifier `get_pr "DB_INST_NAME"` 2>/dev/null | wc -l`" != "0" ]]
 then
 echo "RDS up!"
 else
 echo "RDS down!!!"
 echo "Starting create"
-aws rds create-db-instance --db-instance-identifier $(aws ssm get-parameters --names DB_INST_NAME --with-decryption --output text | awk '{print $4}') \
---allocated-storage 5 --db-instance-class db.t2.micro --engine postgres \
---master-username $(aws ssm get-parameters --names DB_USER --with-decryption --output text | awk '{print $4}') \
---master-user-password $(aws ssm get-parameters --names DB_PASS --with-decryption --output text | awk '{print $4}') \
---storage-type gp2 --backup-retention-period 0 --db-name $(aws ssm get-parameters --names DB_NAME --with-decryption --output text | awk '{print $4}')
+aws rds create-db-instance --db-instance-identifier `get_pr "DB_INST_NAME"` --allocated-storage 5 --db-instance-class db.t2.micro --engine postgres \
+--master-username `get_pr "DB_USER"` --master-user-password `get_pr "DB_PASS"` --storage-type gp2 --backup-retention-period 0 --db-name `get_pr "DB_NAME"`
 #======================================
 # Checking create DB
 #
 TARGET_STATUS=available
 STATUS=unknown
 while [[ "$STATUS" != "$TARGET_STATUS" ]]; do
-        STATUS=`aws rds describe-db-instances --db-instance-identifier $(aws ssm get-parameters --names DB_INST_NAME --with-decryption --output text | awk '{print $4}') | grep DBInstanceStatus | awk '{print$2}' | cut -d'"' -f2`
+        STATUS=`aws rds describe-db-instances --db-instance-identifier `get_pr "DB_INST_NAME"` | grep DBInstanceStatus | awk '{print$2}' | cut -d'"' -f2`
         echo "Database $INSTANCE : $STATUS ... "
         sleep 15
 done
 #======================================
 # Set DB variables
 #
-EXISTING_DB_INSTANCE_INFO=`aws rds describe-db-instances --db-instance-identifier $(aws ssm get-parameters --names DB_INST_NAME --with-decryption --output text | awk '{print $4}') --query 'DBInstances[*].[DBInstanceIdentifier,Endpoint.Address,Endpoint.Port]' --output text`
+EXISTING_DB_INSTANCE_INFO=aws rds describe-db-instances --db-instance-identifier `get_pr "DB_INST_NAME"` \
+--query 'DBInstances[*].[DBInstanceIdentifier,Endpoint.Address,Endpoint.Port]' --output text
 aws ssm put-parameter --name "DB_HOST" --type "String" --value "$(echo ${EXISTING_DB_INSTANCE_INFO} | awk '{print $2}')" --overwrite
 aws ssm put-parameter --name "DB_PORT" --type "String" --value "$(echo ${EXISTING_DB_INSTANCE_INFO} | awk '{print $3}')" --overwrite
 fi
